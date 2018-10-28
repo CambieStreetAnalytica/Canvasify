e0fb252a3bfddcb2a604eba3040421a94b9c8b1e
Declan Herbertson <dherbertson@alumni.ubc.ca>
2018-10-28 15:02:45 -0700

    added dummy stats section

--- web/src/app/avatar-stat-summary/avatar-stat-summary.control.ts
+++ web/src/app/avatar-stat-summary/avatar-stat-summary.control.ts
@@ -3,10 +3,4 @@ export class AvatarStatSummaryControl {
 
     // inject canvasService here
     constructor() {}
-
-    /*
-    public avatarStats(): any {
-        return this.canvasService.avatarStats();
-    }
-    */
 }
