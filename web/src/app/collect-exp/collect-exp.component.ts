7cafa43a2d88468ad8159c9dddf79cea5bddd9e7
Declan Herbertson <dherbertson@alumni.ubc.ca>
2018-11-11 22:29:55 -0800

    restructuring and styling app

--- web/src/app/collect-exp/collect-exp.component.ts
+++ web/src/app/collect-exp/collect-exp.component.ts
@@ -1,26 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { CanvasService } from '../canvas.service';
-import { Submission } from '../interfaces/submission';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import {Component, OnInit} from '@angular/core';
+import {CanvasService} from '../canvas.service';
+import {Submission} from '../interfaces/submission';
+import {Observable} from 'rxjs';
 
 @Component({
-  selector: 'app-collect-exp',
-  templateUrl: './collect-exp.component.html',
-  styleUrls: ['./collect-exp.component.less']
+    selector: 'app-collect-exp',
+    templateUrl: './collect-exp.component.html',
+    styleUrls: ['./collect-exp.component.less']
 })
 export class CollectExpComponent implements OnInit {
-  public submissions: Observable<Submission[]>;
+    public mockSubmissions: Submission[];
+    public submissions: Observable<Submission[]>;
 
-  constructor(public canvasService: CanvasService) {
-  }
+    constructor(public canvasService: CanvasService) {
+    }
 
-  ngOnInit() {
-    this.submissions = this.canvasService.getAllSubmissionsObservable();
-  }
+    ngOnInit() {
+        this.mockSubmissions = [
+            {name: "Midterm 1", score: 30} as Submission,
+            {name: "Quiz 6", score: 20} as Submission,
+            {name: "Assignment 4", score: 15} as Submission
+        ];
+        this.submissions = this.canvasService.getAllSubmissionsObservable();
+    }
 
-  public onClick(submission: Submission): void {
-    this.canvasService.setExperiencePoints(submission.score);
-  }
+    public onClick(submission: Submission): void {
+        this.canvasService.setExperiencePoints(submission.score);
+    }
 
 }
